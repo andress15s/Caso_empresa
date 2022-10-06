74f21cf1c97286e7e2df697f1c1fe7a8d3c6b4dd
andress15s <carlos2214104@correo.uis.edu.co>
2022-10-06 08:49:16 -0500

    caso empleado

--- vista/PanelEntradaDatos.java
+++ vista/PanelEntradaDatos.java
@@ -3,7 +3,6 @@ package vista;
 import java.awt.Color;
 import java.awt.Font;
 import javax.swing.BorderFactory;
-import javax.swing.ImageIcon;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
--- vista/VentanaPrincipal.java
+++ vista/VentanaPrincipal.java
@@ -1,6 +1,5 @@
 package vista;
 
-import java.awt.Component;
 
 import javax.swing.JFrame;
 
